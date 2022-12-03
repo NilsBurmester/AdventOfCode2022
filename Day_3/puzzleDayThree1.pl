c4923ce22d749f14500adfcc3de46d3fd6c9ed4f
NilsBurmester <nilsburmester97@outlook.de>
2022-12-03 19:07:03 +0100

    clean up

--- Day_3/puzzleDayThree1.pl
+++ Day_3/puzzleDayThree1.pl
@@ -13,7 +13,7 @@ my %priorities =    ("a" => 1, "b" => 2, "c" => 3, "d" => 4, "e" => 5, "f" => 6,
                      "A" => 27, "B" => 28, "C" => 29, "D" => 30, "E" => 31, "F" => 32, "G" => 33, "H" => 34, "I" => 35, "J" => 36, "K" => 37,
                      "L" => 38, "M" => 39, "N" => 40, "O" => 41, "P" => 42, "Q" => 43, "R" => 44, "S" => 45, "T" => 46, "U" => 47, "V" => 48,
                      "W" => 49, "X" => 50, "Y" => 51, "Z" => 52, "no common" => 0); 
-my $priority = 0;
+my $prioritySum = 0;
 
 foreach (@contents) {
         chomp $_;
@@ -22,12 +22,12 @@ foreach (@contents) {
         my $secondCompartment = substr($_, $length/2, $length/2);
         my $common = common($firstCompartment, $secondCompartment);
 
-        $priority += $priorities{$common};
+        $prioritySum += $priorities{$common};
         print "$firstCompartment\t";
         print "$priorities{$common}\t";
         print "$secondCompartment\n";
 }
-print $priority;
+print $prioritySum;
 
 sub common {
    (my $firstHalf, my $secondHalf) = @_; 
@@ -48,5 +48,3 @@ sub common {
     return "no common";
 }
 
-FGfFDBhGGlfGGf
-WJWdbSRSnRNbTvdn
